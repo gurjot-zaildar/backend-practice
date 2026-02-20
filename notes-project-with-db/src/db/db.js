84cf99d2217722679826d8033add7cefce324d77
GURJOT SINGH <signingames115@gmail.com>
2026-02-20 15:28:41 +0530

    Change DB connection string to placeholder
    Updated database connection string to use a placeholder.

--- notes-project-with-db/src/db/db.js
+++ notes-project-with-db/src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-function connectToDB(){ mongoose.connect("mongodb+srv://signingames115_db_user:9Wb688Z3JBYZLVGw@cluster0.41wquta.mongodb.net/cohort")
+function connectToDB(){ mongoose.connect("url of DB")
 .then(()=>{
 console.log("server is connected to db")
 })
